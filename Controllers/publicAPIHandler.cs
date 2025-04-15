9d98847ba9e3f817e5ea75258296fffafd437693
senthilsois <senthilkumar.s@officeinfosystems.com>
2025-04-15 18:20:09 +0530

    initial commit of code for rabbitmq setup

---
+++ Controllers/publicAPIHandler.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+using System.Data;
+using System.Data.SqlClient;
+namespace OISPublic.Controllers
+{
+    public class publicAPIHandler : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public publicAPIHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        //Get API to create the external link
+        [HttpGet]
+        [Route("/public_link/{documentId}/{shareId}")]
+        public async Task<IActionResult> GeneratePublicLinkHandler(string documentId, string shareId)
+        {
+            using var connection = new SqlConnection(_configuration.GetConnectionString("oispublicdb_connectionstring"));
+            await connection.OpenAsync();
+
+            if (!string.IsNullOrEmpty(documentId))
+            {
+                var affectedRows = await connection.ExecuteAsync(
+                    "UPDATE DocumentAuditTrails SET OperationName = @OperationName WHERE DocumentId = @DocumentId",
+                    new { OperationName = 1, DocumentId = documentId }
+                );
+                return Ok(affectedRows);
+            }
+            else
+            {
+                return BadRequest("Document id is not valid!");
+            }
+        }
+    }
+}
