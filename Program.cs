861655a7b96e7cc14ef9b5da0d778eb96d3895a4
mahesh2399 <110028205+mahesh2399@users.noreply.github.com>
2025-07-09 10:59:22 +0530

    allow orgin

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using OISPublic.Services;
 using DocumentManagement.Helper;
+using Microsoft.Graph.Models.ExternalConnectors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -61,7 +62,15 @@ builder.Services.AddDbContext<OISDataRoomContext>((serviceProvider, options) =>
     options.UseSqlServer(connectionString);
 });
 
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 // Add services to the container.
 
@@ -79,16 +88,10 @@ var app = builder.Build();
 // Enable Swagger in all environments
 app.UseSwagger();
 app.UseSwaggerUI();
-//app.UseSwaggerUI(c =>
-//{
-//    //c.SwaggerEndpoint("/public_a/swagger/v1/swagger.json", "public");
-//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "");
-//    c.RoutePrefix = "swagger"; // optional, ensures it's at /swagger
-//});
 
 
 
-app.UseCors();
+app.UseCors("AllowAll");
 
 app.UseMiddleware<ConnectionStringMiddleware>();
 app.UseAuthentication();
@@ -97,46 +100,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+builder.Services.AddDbContext<OISDataRoomContext>((serviceProvider, options) =>
+{
+    var provider = serviceProvider.GetRequiredService<ConnectionStringProvider>();
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+
+    var connectionStringName = provider.ConnectionStringName;
+    var connectionString = configuration.GetConnectionString(connectionStringName);
+
+    if (string.IsNullOrEmpty(connectionString))
+        throw new InvalidOperationException("The ConnectionString property has not been initialized.");
+
+    options.UseSqlServer(connectionString);
+});
 
 
-// Fix for CS0234: Ensure the required NuGet package is installed and the namespace is correctly referenced.
-// The 'Microsoft.Identity.Web.UI' namespace is part of the 'Microsoft.Identity.Web' package, but it requires the 'Microsoft.Identity.Web.UI' assembly.
-// Install the package via NuGet if not already installed:
-//// Command: dotnet add package Microsoft.Identity.Web.UI
-//using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-//using Microsoft.Identity.Web;
-//using Microsoft.Identity.Web.UI;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Configure authentication with Azure AD
-//builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-//    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
-//    .EnableTokenAcquisitionToCallDownstreamApi()
-//    .AddMicrosoftGraph(builder.Configuration.GetSection("Graph"))
-//    .AddInMemoryTokenCaches();
-
-//// Authorization setup
-//builder.Services.AddAuthorization(options =>
-//{
-//    options.FallbackPolicy = options.DefaultPolicy;
-//});
-
-//// Register Graph API + other services
-//builder.Services.AddHttpClient();
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-//var app = builder.Build();
-
-//// Middleware
-//app.UseSwagger();
-//app.UseSwaggerUI();
-
-//app.UseHttpsRedirection();
-//app.UseAuthentication();  // IMPORTANT: This must come before UseAuthorization
-//app.UseAuthorization();
-
-//app.MapControllers();
-//app.Run();
