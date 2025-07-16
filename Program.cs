b6583e6114d75ca94e93f25906c9325b4763ad53
mahesh2399 <110028205+mahesh2399@users.noreply.github.com>
2025-07-16 23:51:28 +0530

    chnages ifor all environment issues

--- Program.cs
+++ Program.cs
@@ -8,19 +8,32 @@ using OISPublic.Services;
 using DocumentManagement.Helper;
 using Microsoft.Graph.Models.ExternalConnectors;
 using OISPublic.Helper;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Load JWT settings
 var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 var secretKey = jwtSettings["SecretKey"];
 
+// --- Service Configuration ---
+builder.Services.AddScoped<ConnectionStringProvider>();
 
+builder.Services.AddDbContext<OISDataRoomContext>((serviceProvider, options) =>
+{
+    var provider = serviceProvider.GetRequiredService<ConnectionStringProvider>();
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
 
+    var connectionStringName = provider.ConnectionStringName;
+    var connectionString = configuration.GetConnectionString(connectionStringName);
 
+    if (string.IsNullOrEmpty(connectionString))
+        throw new InvalidOperationException("The ConnectionString property has not been initialized.");
 
+    options.UseSqlServer(connectionString);
+});
 
-
+// JWT Authentication
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -42,45 +55,21 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
-
-
-
-
-
-builder.Services.AddScoped<ConnectionStringProvider>();
-
-builder.Services.AddDbContext<OISDataRoomContext>((serviceProvider, options) =>
-{
-    var provider = serviceProvider.GetRequiredService<ConnectionStringProvider>();
-    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-
-    var connectionStringName = provider.ConnectionStringName;
-    var connectionString = configuration.GetConnectionString(connectionStringName);
-
-    if (string.IsNullOrEmpty(connectionString))
-        throw new InvalidOperationException("The ConnectionString property has not been initialized.");
-
-    options.UseSqlServer(connectionString);
-});
-
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", builder =>
+    options.AddPolicy("AllowAll", policy =>
     {
-        builder.AllowAnyOrigin()
-               .AllowAnyMethod()
-               .AllowAnyHeader();
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
     });
 });
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddHttpClient(); // For .NET 6+
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddHttpClient();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<JwtTokenService>();
 builder.Services.AddAuthorization();
 builder.Services.AddSingleton<PathHelper>();
@@ -88,32 +77,51 @@ builder.Services.AddSignalR();
 builder.Services.AddHostedService<NotificationWatcherService>();
 builder.Services.AddScoped<NotificationService>();
 
-
 var app = builder.Build();
 
-// Enable Swagger in all environments
-app.UseStaticFiles();
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+// --- Global Error Handling ---
+app.UseExceptionHandler(errorApp =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    errorApp.Run(async context =>
+    {
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "application/json";
+
+        var exceptionFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+        var exception = exceptionFeature?.Error;
+
+        var errorDetails = new
+        {
+            Message = "A server error occurred.",
+            ExceptionMessage = exception?.Message,
+            ExceptionType = exception?.GetType().Name,
+            StackTrace = exception?.StackTrace,
+            InnerException = exception?.InnerException?.Message,
+            Path = exceptionFeature?.Path
+        };
+
+        await context.Response.WriteAsJsonAsync(errorDetails);
+    });
 });
+
+
+app.UseStaticFiles();
+app.UseSwagger(); 
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
 
 app.Use(async (context, next) =>
 {
-  if(context.Request.Path == "/")
+    if (context.Request.Path == "/")
     {
-
         context.Response.Redirect("/swagger/index.html");
         return;
-
     }
     await next();
 });
 
-
 app.UseCors("AllowAll");
-
 app.UseMiddleware<ConnectionStringMiddleware>();
 app.UseAuthentication();
 app.UseAuthorization();
@@ -122,18 +130,3 @@ app.MapControllers();
 app.MapHub<NotificationHub>("/hubs/notifications");
 
 app.Run();
-builder.Services.AddDbContext<OISDataRoomContext>((serviceProvider, options) =>
-{
-    var provider = serviceProvider.GetRequiredService<ConnectionStringProvider>();
-    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
-
-    var connectionStringName = provider.ConnectionStringName;
-    var connectionString = configuration.GetConnectionString(connectionStringName);
-
-    if (string.IsNullOrEmpty(connectionString))
-        throw new InvalidOperationException("The ConnectionString property has not been initialized.");
-
-    options.UseSqlServer(connectionString);
-});
-
-
